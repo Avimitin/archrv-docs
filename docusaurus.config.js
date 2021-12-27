c3b710d93bc990a2468c65bb7620046a47c4a063
Avimitin <avimitin@gmail.com>
2021-12-27 14:15:12 +0800

    rwt,config: replace edit url to my repository
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/Avimitin/RISC-V_Daily_Notes',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Avimitin/RISC-V_Daily_Notes',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
